3cab4306c898db765da163a695e0261975796ef7
Andy Tran <andyhqtran@gmail.com>
2015-03-03 16:57:44 -0800

    Updated Header & CSS file. Added in responsive styling.

--- header.php
+++ header.php
@@ -18,41 +18,41 @@
 	    <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_directory'); ?>/img/apple-touch-icon-72x72.png">
 	    <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/img/apple-touch-icon-114x114.png">
 
-	<?php wp_head(); ?>
+		<?php wp_head(); ?>
 	</head>
 
 	<body <?php body_class(); ?>>
 		<div class='hero'>
-		  <header class='header'>
-		    <div class='container'>
-		      <div class='col-md-12'>
-		        <div class='navbar clearfix'>
-		          <h2 class='logo'>
-		            <a href='<?php echo esc_url( home_url( '/' ) ); ?>'><?php bloginfo( 'name' ); ?></a>
-		          </h2>
-		          <nav class='nav'>
-		          	<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		          </nav>
-		        </div>
-		      </div>
-		    </div>
-		  </header>
-		  <?php if( is_home() || is_front_page() ) : ?>
-		  <div class='hero-text' data-sr='scale up 30%'>
-		    <div class='container'>
-		      <div class='row'>
-		        <div class='col-md-12'>
-		          <h2>Two heads are better than one.</h2>
-		          <p>We need to think of a sick tagline right here, because I can’t think of shit right now</p>
-		          <div class='btn-group' data-sr='enter bottom wait .3s ease 60px'>
-		            <a class='btn btn-hero' href='#featured'>
-		              <div class='fa fa-long-arrow-down'></div>
-		            </a>
-		            <span>See our case studies</span>
-		          </div>
-		        </div>
-		      </div>
-		    </div>
-		  </div>
-<?php endif; ?>
+		  	<header class='header'>
+		    	<div class='container'>
+			      	<div class='col-md-12'>
+			        	<div class='navbar clearfix'>
+			          		<h2 class='logo'>
+				            	<a href='<?php echo esc_url( home_url( '/' ) ); ?>'><?php bloginfo( 'name' ); ?></a>
+				          	</h2>
+			          		<nav class='nav'>
+			          			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			          		</nav>
+			        	</div>
+			      	</div>
+			    </div>
+		  	</header>
+		  	<?php if( is_home() || is_front_page() ) : ?>
+		  	<div class='hero-text' data-sr='scale up 30%'>
+		    	<div class='container'>
+		      		<div class='row'>
+		        		<div class='col-md-12'>
+		          			<h2>Two heads are better than one.</h2>
+		          			<p>We need to think of a sick tagline right here, because I can’t think of shit right now</p>
+		          			<div class='btn-group' data-sr='enter bottom wait .3s ease 60px'>
+		            			<a class='btn btn-hero' href='#featured'>
+		              				<div class='fa fa-long-arrow-down'></div>
+		            			</a>
+		            			<span>See our case studies</span>
+		          			</div>
+		        		</div>
+		      		</div>
+		    	</div>
+		  	</div>
+			<?php endif; ?>
 		</div>
