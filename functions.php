58862cb87430cadb60368f65502d5ee516185fac
Andy Tran <andyhqtran@gmail.com>
2015-03-06 02:01:33 -0800

    Removed unnecessary scripts.

--- functions.php
+++ functions.php
@@ -70,10 +70,6 @@ function gently_scripts() {
     wp_enqueue_style( 'google_fonts_css', 'http://fonts.googleapis.com/css?family=Varela+Round');
     wp_enqueue_style( 'style_css', get_stylesheet_uri() );
 
-    wp_enqueue_script( 'gently-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-    wp_enqueue_script( 'gently-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
     wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.2', true);
     wp_enqueue_script( 'scrollReveal_js', get_template_directory_uri() . '/js/scrollReveal.min.js', array('jquery'), '2.1.0', true);
     wp_enqueue_script( 'script_js', get_template_directory_uri() . '/js/script.js', array('jquery', 'bootstrap_js'), '0.1', true);
